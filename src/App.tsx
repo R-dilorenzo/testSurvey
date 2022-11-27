66b326258fac677b39c4034b3e649a63bfbb2cb7
dilorenzo9 <47335398+dilorenzo9@users.noreply.github.com>
2022-11-27 09:07:17 +0100

    clean code

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,6 @@ function App() {
 	return (
 		<div>
 			<Routes />
-			{/* Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim minus perferendis magni tenetur aliquam,
-			pariatur cum officia quis quaerat consequuntur minima quisquam nam debitis dicta veritatis! Sed, voluptates
-			consequatur! Itaque.
-			<Button>some txt</Button>
-			<Button disabled>some txt</Button>
-			<Button loading>some txt</Button>
-			<Button block>some txt</Button>
-			<br />
-			<Input />
-			<Input label="test label" />
-			<Input label="test label" clearable />
-			<br />
-			<Button onClick={() => setVisible(!visible)}>toggle</Button>
-			<Collapse isShow={visible} collapseDuration={250}>
-				text
-			</Collapse> */}
 		</div>
 	);
 }
